b1d7e380658d3a745a9dce78a18c0fbea602922e
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-03-29 19:07:41 +0300

    all

--- src/push_swap.c
+++ src/push_swap.c
@@ -15,6 +15,8 @@
 #include <limits.h>
 #include <stdio.h>
 
+void	push_swap(t_psab *stacks);
+
 static const int	cmpA(size_t current, size_t next)
 {
 	return (current < next);
@@ -212,6 +214,8 @@ size_t	findMax(t_list *lst)
 {
 	size_t	max;
 
+	if (!lst)
+		return (0);
 	max = 0;
 	while (lst)
 	{
@@ -220,6 +224,22 @@ size_t	findMax(t_list *lst)
 		lst = lst->next;
 	}
 	return (max);
+}
+
+size_t	findMin(t_list *lst)
+{
+	size_t	min;
+
+	if (!lst)
+		return (0);
+	min = ((t_ps *)lst->content)->index;
+	while (lst)
+	{
+		if (min > ((t_ps *)lst->content)->index)
+			min = ((t_ps *)lst->content)->index;
+		lst = lst->next;
+	}
+	return (min);
 }
 
 void	movAsort(t_psab *stacks)
@@ -243,25 +263,99 @@ void	sortA(t_psab *stacks)
 {
 	while (stacks->a->next)
 	{
-		if (((t_ps *)stacks->a->next->content)->index > ((t_ps *)stacks->a->content)->index)
+		// printf("index: %zu\n", ((t_ps *)stacks->a->content)->index);
+		if (stacks->lsindex == ((t_ps *)stacks->a->content)->index)
+		{
 			ps_ra(stacks);
+			stacks->lsindex++;
+		}
 		else
 			break;
 	}
 }
 
+void	backtrack(t_psab *stacks, size_t max)
+{
+	while (((t_ps *)stacks->a->content)->index <= max && ((t_ps *)stacks->a->content)->index != 1)
+	{
+		if (((t_ps *)stacks->a->content)->index == stacks->lsindex)
+		{
+			ps_ra(stacks);
+			stacks->lsindex++;
+		}
+		else
+			ps_pb(stacks);
+	}
+	if (findMin(stacks->b) == stacks->lsindex)
+		push_swap(stacks);	
+}
+
+void	backtrack_split(t_psab *stacks, size_t max)
+{
+	size_t	median;
+	size_t	count;
+	size_t	i;
+
+	count = 0;
+	median = find_median(stacks->a, stacks->size_a - stacks->lsindex);
+	while (((t_ps *)stacks->a->content)->index <= max && ((t_ps *)stacks->a->content)->index != 1)
+	{
+		if (((t_ps *)stacks->a->content)->index >= median)
+		{
+			ps_ra(stacks);
+			count++;
+		}
+		else
+			ps_pb(stacks);
+	}
+	i = -1;
+	while (++i < count)
+		if (((t_ps *)stacks->b->content)->index != findMax(stacks->b))
+			ps_rrr(stacks);
+		else
+			ps_rra(stacks);
+	if (findMin(stacks->b) == stacks->lsindex && count > 0)
+		push_swap(stacks);
+}
+
+size_t	get_notsort_size(t_list *lst, size_t max)
+{
+	size_t count;
+
+	if (!lst)
+		return (0);
+	count = 0;
+	while (lst && ((t_ps *)lst->content)->index <= max && ((t_ps *)lst->content)->index != 1)
+	{
+		count++;
+		lst = lst->next;
+	}
+	return (count);
+}
+
 void	push_swap(t_psab *stacks)
 {
 	size_t	median;
+	size_t	max;
+	size_t	size;
+
 	
 	if (stacks->size_b == 0)
 		return ;
+	max = findMax(stacks->b);
 	median = find_median(stacks->b, stacks->size_b);
-	while (stacks->size_b > 12)
-		ps_movA(stacks, 12);
-	
-	// movAsort(stacks);
-	// sortA(stacks);
+	size = stacks->size_b;
+	while (size--)
+	{
+		if (stacks->size_b < 13)
+			break;
+		ps_movA(stacks, median);
+	}
+	movAsort(stacks);
+	sortA(stacks);
+	if (get_notsort_size(stacks->a, max) >= 20)
+		backtrack_split(stacks, max);
+	backtrack(stacks, max);
 }
 
 int	main(int ac, char **av)
@@ -273,7 +367,7 @@ int	main(int ac, char **av)
 	if (!(stacks.a = ps_crtlst(av + 1, ac - 1)))
 		throw_error();
 	stacks.b = NULL;
-	stacks.debug = 1;
+	stacks.debug = 0;
 	stacks.color = 1;
 	stacks.lsindex = 1;
 	stacks.size_a = ps_giveindex(stacks.a);
@@ -281,6 +375,11 @@ int	main(int ac, char **av)
 	stacks.size = stacks.size_a;
 	ps_movB(&stacks);
 	push_swap(&stacks);
-
+	if (get_notsort_size(stacks.a, findMax(stacks.a)) >= 20)
+	{
+		backtrack_split(&stacks, findMax(stacks.a));
+		backtrack_split(&stacks, findMax(stacks.a));
+	}
+	backtrack(&stacks, findMax(stacks.a));
 	return (0);
 }
